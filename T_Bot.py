9c38922b5de24f553407ea2bba0e5e411357dce6
frederickl-1 <96441364+frederickl-1@users.noreply.github.com>
2022-02-26 12:11:44 +0000

    Update T_Bot.py

--- T_Bot.py
+++ T_Bot.py
@@ -10,7 +10,7 @@ from twilio.rest import Client
 # Sirius KuCoin Test Net
 
 
-api_key = "6215534b29c69200011e0027"
+api_key = os.environ.get('KC_API_KEY') #api_key = "6215534b29c69200011e0027"
 api_secret = "8b25ab7f-2a50-44de-8ffc-f0a430040aca"
 api_passphrase = "6NAcxQ#gob!$!FUAf4j6#JcooX%&f"
 
