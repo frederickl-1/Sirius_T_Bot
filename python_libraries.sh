a4c580e5f1a874adab251266ec3b379bc7ef4f9a
frederickl-1 <96441364+frederickl-1@users.noreply.github.com>
2022-02-26 11:28:21 +0000

    Update python_libraries.sh

--- python_libraries.sh
+++ python_libraries.sh
@@ -5,5 +5,5 @@ echo "Installing pip"
 sudo apt install python3-pip
 echo "Installing KuCoin SDK"
 pip install kucoin-python
-echo "Installing Twillio"
-pip install twillio
+echo "Installing Twilio"
+pip install twilio
